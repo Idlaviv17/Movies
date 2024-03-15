def1e27f6ffacc418d04a5b776aa354e7b0c6b1c
Idlaviv <jjavier1708@gmail.com>
2024-03-15 16:25:55 -0700

    Add firebase auth

--- app/src/main/java/angulo/javier/movies/LoginActivity.kt
+++ app/src/main/java/angulo/javier/movies/LoginActivity.kt
@@ -3,13 +3,23 @@ package angulo.javier.movies
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var auth : FirebaseAuth
     lateinit var btn_login: Button
     lateinit var btn_signup: Button
     lateinit var tv_recovery: TextView
+    lateinit var et_email: EditText
+    lateinit var et_password: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -17,10 +27,39 @@ class LoginActivity : AppCompatActivity() {
         btn_login = findViewById(R.id.btn_login)
         btn_signup = findViewById(R.id.btn_signup)
         tv_recovery = findViewById(R.id.tv_recovery)
+        et_email = findViewById(R.id.et_email_login)
+        et_password = findViewById(R.id.et_password_login)
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
 
         btn_login.setOnClickListener {
-            var intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+            var email: String = et_email.text.toString()
+            var password: String = et_password.text.toString()
+
+            if(!email.isNullOrEmpty() && !password.isNullOrEmpty()) {
+
+                auth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            // Sign in success, update UI with the signed-in user's information
+                            val user = auth.currentUser
+                            var intent = Intent(this, MainActivity::class.java)
+                            startActivity(intent)
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(
+                                this,
+                                "Incorrect data.",
+                                Toast.LENGTH_SHORT,
+                            ).show()
+                        }
+                    }
+            } else {
+                Toast.makeText(this, "Ingresar datos", Toast.LENGTH_SHORT)
+            }
+
+
         }
 
         btn_signup.setOnClickListener {
--- app/src/main/java/angulo/javier/movies/RegisterActivity.kt
+++ app/src/main/java/angulo/javier/movies/RegisterActivity.kt
@@ -2,10 +2,74 @@ package angulo.javier.movies
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var auth : FirebaseAuth
+    lateinit var et_email : EditText
+    lateinit var et_password1 : EditText
+    lateinit var et_password2 : EditText
+    lateinit var btn_register : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        et_email = findViewById(R.id.et_email_register)
+        et_password1 = findViewById(R.id.et_password1_register)
+        et_password2 = findViewById(R.id.et_password2_register)
+        btn_register = findViewById(R.id.btn_register_layout)
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
+        btn_register.setOnClickListener {
+            var email: String = et_email.text.toString()
+            var password1: String = et_password1.text.toString()
+            var password2: String = et_password2.text.toString()
+
+            if (!email.isNullOrEmpty() && !password1.isNullOrEmpty() && !password2.isNullOrEmpty()) {
+
+                if (password1 == password2) {
+
+                    auth.createUserWithEmailAndPassword(email, password1)
+                        .addOnCompleteListener(this) { task ->
+                            if (task.isSuccessful) {
+                                val user = auth.currentUser
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d("success", "createUserWithEmail:success")
+                                Toast.makeText(
+                                    this,
+                                    "User ${user?.email} has been registered.",
+                                    Toast.LENGTH_SHORT,
+                                ).show()
+                                finish()
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w("error", "createUserWithEmail:failure", task.exception)
+                                Toast.makeText(
+                                    this,
+                                    "Authentication failed.",
+                                    Toast.LENGTH_SHORT,
+                                ).show()
+                            }
+                        }
+
+                } else {
+                    Toast.makeText(this, "Passwords do not match", Toast.LENGTH_SHORT).show()
+                }
+
+            } else {
+                Toast.makeText(this, "Enter email and password", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
 }
